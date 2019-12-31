cf86c2dd4d1f6ba6e9f30c87ada5b7bb768c92a8
Tyler Parsotan <parsotat@oregonstate.edu>
2019-12-31 16:43:51 -0500

    Added changes from main branch for testing.

--- mcrat.c
+++ mcrat.c
@@ -748,7 +748,17 @@ int main(int argc, char **argv)
                     
                     fprintf(fPtr,">>\n");
                     fprintf(fPtr,">> Proc %d with angles %0.1lf-%0.1lf: Working on photons injected at frame: %d out of %d\n", angle_id, theta_jmin_thread*180/M_PI, theta_jmax_thread*180/M_PI,frame, frm2);
-                    fprintf(fPtr,">> Proc %d with angles %0.1lf-%0.1lf: Simulation type %d - Working on frame %d\n",angle_id, theta_jmin_thread*180/M_PI, theta_jmax_thread*180/M_PI, SIMULATION_TYPE, scatt_frame);
+                    
+                    #if SIMULATION_TYPE == SCIENCE
+                        fprintf(fPtr,">> Proc %d with angles %0.1lf-%0.1lf: Simulation type Science - Working on frame %d\n",angle_id, theta_jmin_thread*180/M_PI, theta_jmax_thread*180/M_PI, scatt_frame);
+                    #elif SIMULATION_TYPE == SPHERICAL_OUTFLOW
+                        fprintf(fPtr,">> Proc %d with angles %0.1lf-%0.1lf: Simulation type Spherical Outflow - Working on frame %d\n",angle_id, theta_jmin_thread*180/M_PI, theta_jmax_thread*180/M_PI, scatt_frame);
+                    #elif SIMULATION_TYPE == CYLINDRICAL_OUTFLOW
+                        fprintf(fPtr,">> Proc %d with angles %0.1lf-%0.1lf: Simulation type Cylindrical Outflow - Working on frame %d\n",angle_id, theta_jmin_thread*180/M_PI, theta_jmax_thread*180/M_PI, scatt_frame);
+                    #elif SIMULATION_TYPE == STRUCTURED_SPHERICAL_OUTFLOW
+                        fprintf(fPtr,">> Proc %d with angles %0.1lf-%0.1lf: Simulation type Structurd Spherical Outflow - Working on frame %d\n",angle_id, theta_jmin_thread*180/M_PI, theta_jmax_thread*180/M_PI, scatt_frame);
+                    #endif
+                    
                     fprintf(fPtr,">> Proc %d with angles %0.1lf-%0.1lf: Opening file...\n", angle_id, theta_jmin_thread*180/M_PI, theta_jmax_thread*180/M_PI);
                     fflush(fPtr);
                     
