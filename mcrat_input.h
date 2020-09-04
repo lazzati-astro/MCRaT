39a021f4de029a7fe4d173b7c7d00e03c35258fa
Tyler Parsotan <parsotat@oregonstate.edu>
2020-09-03 21:43:21 -0700

    Cleaned up mcrat_input file.

--- mcrat_input.h
+++ mcrat_input.h
@@ -1,51 +1,17 @@
 //
 //  mcrat_input.h
-//
+//  This file serves as an input of the various types of physics that MCRaT should consider.
+//  as well as the microphysical considerations that go along with those physics. Various
+//  options are outlined in the documentation.
 //
 //  Created by Tyler Parsotan on 12/9/19.
 //
 
-
-
-//MODIFY C COMPILER DIRECTIVES BELOW THIS LINE
-
 #define SIMULATION_TYPE SPHERICAL_OUTFLOW
-//#define FILEPATH "/Volumes/LACIE_RAID/Collapsars/2D/HUGE_BOXES/CONSTANT/16OI/"
 #define FILEPATH "/Users/Tylerparsotan/Documents/16OI_TEST/"
 #define FILEROOT "rhd_jet_big_16OI_hdf5_plt_cnt_"
-//#define MC_PATH "16OI_SPHERICAL/"
 #define MC_PATH "SKN_16OI_SPHERICAL/"
 
-/*
-#define THISRUN "Science"
-#define FILEPATH "/Volumes/LACIE_RAID/Collapsars/2D/HUGE_BOXES/CONSTANT/16TI/"
-#define FILEROOT "rhd_jet_big_13_hdf5_plt_cnt_"
-#define MC_PATH "KN_CMC_16TI/"
-
- #define    SIMULATION_TYPE         SCIENCE
- #define    FILEPATH                "/Users/parsotat/Downloads/"
- #define    FILEROOT                "data."
- #define    MC_PATH                 "PLUTO_MCRAT/"
- 
-
- #define THISRUN "Science"
- #define FILEPATH "/home/physics/parsotat/16OM/"
- #define FILEROOT "rhd_jet_big_16OM_hdf5_plt_cnt_"
- #define MC_PATH "DIR_TEST/"
- 
- #define THISRUN "Spherical"
- #define FILEPATH "/Volumes/DATA6TB/Collapsars/2D/HUGE_BOXES/CONSTANT/16OI/"
- //#define FILEPATH "/Users/Tylerparsotan//Documents/16OI_TEST/"
- #define FILEROOT "rhd_jet_big_16OI_hdf5_plt_cnt_"
- #define MC_PATH "TEST/"
- 
- #define THISRUN "Spherical"
- //#define THISRUN "Structured Spherical"
- //#define FILEPATH "/home/physics/parsotat/16TI/"
- #define FILEPATH "/Users/Tylerparsotan/Documents/16TI/"
- #define FILEROOT "rhd_jet_big_13_hdf5_plt_cnt_"
- #define MC_PATH "KN_CMC_16TI_SPHERICAL/"
-*/
 
 #define     SIM_SWITCH              FLASH
 #define     STOKES_SWITCH           ON
@@ -54,8 +20,6 @@
 #define     GEOMETRY                CARTESIAN
 #define     HYDRO_L_SCALE           1e9
 #define     SYNCHROTRON_SWITCH      ON
-#define     B_FIELD_CALC            TOTAL_E
-#define     EPSILON_B               0.5
 #define     SAVE_TYPE               ON
 
 #define     MCPAR                   "mc.par"
