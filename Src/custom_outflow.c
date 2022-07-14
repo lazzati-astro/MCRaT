54cc6ef7906e9ccf7f31a184e7cf01fdf136aef1
Tyler Parsotan <parsotat@oregonstate.edu>
2022-07-14 11:11:15 -0400

    Wrote function with custom outflow.

--- Src/custom_outflow.c
+++ Src/custom_outflow.c
@@ -9,18 +9,26 @@
 
 void customOutflowPrep(struct hydro_dataframe *hydro_data, FILE *fPtr)
 {
-    double  gamma_infinity=100, t_comov=1e5, ddensity=3e-7;// the comoving temperature in Kelvin, and the comoving density in g/cm^2
+    double  beta=2.0, t_comov=3*1.63-9/K_B, m_dot_out=0.8, tau_0=2;// the comoving temperature of 3keV in Kelvin, and the initial unitless m_dot, initial optical depth
+    double r_0=1e9; //choose some radius at base of outflow in cm, also is inner limit of simulations
+    double N_0=tau_0*(beta-1)/(THOM_X_SECT*r_0); //initial number density of particles
+    double vel=0, rho=0, r=0, gamma;
     int i=0;
-    double vel=sqrt(1-pow(gamma_infinity, -2.0)), lab_dens=gamma_infinity*ddensity;
     
-    fprintf(fPtr, "The Cylindrical Outflow values are: Gamma_infinity=%e, T_comv=%e K, comv dens=%e g/cm^3 \n", gamma_infinity, t_comov, ddensity);
+    fprintf(fPtr, "The Custom Outflow values are: beta=%e, T_comv=%e K, m_dot=%e, tau_0=%e \n", beta, t_comov, m_dot_out, tau_0);
     fflush(fPtr);
     
     for (i=0; i<hydro_data->num_elements; i++)
     {
-        ((hydro_data->gamma))[i]=gamma_infinity;
-        ((hydro_data->dens))[i]=ddensity;
-        ((hydro_data->dens_lab))[i]=lab_dens;
+        r=((hydro_data->r))[i];
+        vel=C_LIGHT*m_dot_out*pow(r_0/r, 2-beta);
+        rho=N_0*M_P*pow(r_0/r, beta);
+        gamma=1/sqrt(1-pow(vel/C_LIGHT, 2.0));
+        
+        
+        ((hydro_data->gamma))[i]=gamma;
+        ((hydro_data->dens))[i]=rho;
+        ((hydro_data->dens_lab))[i]=rho*gamma;
         ((hydro_data->pres))[i]=(A_RAD*pow(t_comov, 4.0))/(3);
         ((hydro_data->temp))[i]=t_comov; //just assign t_comov
         
@@ -28,15 +36,15 @@ void customOutflowPrep(struct hydro_dataframe *hydro_data, FILE *fPtr)
         #if DIMENSIONS == TWO || DIMENSIONS == TWO_POINT_FIVE
             
             #if GEOMETRY == CARTESIAN || GEOMETRY == CYLINDRICAL
-                ((hydro_data->v0))[i]=0;
-                ((hydro_data->v1))[i]=vel; //geometry dependent want this to be parallel to jet axis
+                ((hydro_data->v0))[i]=(vel*(((hydro_data->r0))[i]))/r;
+                ((hydro_data->v1))[i]=(vel*(((hydro_data->r1))[i]))/r; //geometry dependent want this to be radial
             #endif
 
             #if GEOMETRY == SPHERICAL
-                ((hydro_data->v0))[i]=vel*cos(((hydro_data->r1))[i]);//rhat
-                ((hydro_data->v1))[i]=-vel*sin(((hydro_data->r1))[i]);//theta hat direction
+                ((hydro_data->v0))[i]=vel;//rhat
+                ((hydro_data->v1))[i]=0;//theta hat direction
             #endif
-        
+
             #if DIMENSIONS == TWO_POINT_FIVE
                 //have to make sure that the 3rd vctro direction is set to 0 in 2.5D case
                 ((hydro_data->v2))[i]=0;
@@ -45,22 +53,22 @@ void customOutflowPrep(struct hydro_dataframe *hydro_data, FILE *fPtr)
         #else
 
             #if GEOMETRY == CARTESIAN
-                ((hydro_data->v0))[i]=0;
-                ((hydro_data->v1))[i]=0;
-                ((hydro_data->v2))[i]=vel;
+                ((hydro_data->v0))[i]=(vel*(((hydro_data->r0))[i]))/r;
+                ((hydro_data->v1))[i]=(vel*(((hydro_data->r1))[i]))/r; //geometry dependent want this to be radial
+                ((hydro_data->v2))[i]=(vel*(((hydro_data->r2))[i]))/r;
             #endif
 
 
             #if GEOMETRY == SPHERICAL
-                ((hydro_data->v0))[i]=vel*cos(((hydro_data->r1))[i]);//rhat
-                ((hydro_data->v1))[i]=-vel*sin(((hydro_data->r1))[i]);//theta hat direction
+                ((hydro_data->v0))[i]=vel;//rhat
+                ((hydro_data->v1))[i]=0;//theta hat direction
                 ((hydro_data->v2))[i]=0;
             #endif
 
             #if GEOMETRY == POLAR
-                ((hydro_data->v0))[i]=0;
+                ((hydro_data->v0))[i]=(vel*(((hydro_data->r0))[i]))/r; //need to figure this out
                 ((hydro_data->v1))[i]=0;
-                ((hydro_data->v2))[i]=vel;
+                ((hydro_data->v2))[i]=(vel*(((hydro_data->r2))[i]))/r;
             #endif
 
         #endif
