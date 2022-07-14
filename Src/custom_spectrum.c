a2f7ae2f18c40756c6d14af3542f45a5a3f87032
Tyler Parsotan <parsotat@oregonstate.edu>
2022-07-14 11:33:33 -0400

    Added gaussian function for custom spectrum.

--- Src/custom_spectrum.c
+++ Src/custom_spectrum.c
@@ -6,10 +6,14 @@
 //
 
 #include "mcrat.h"
+#include <gsl/gsl_sf_exp.h>
 
 double custom_spectrum(double frequency)
 {
+    double std=0.2*1.60218e-9/PL_CONST, mean=6.4*1.60218e-9/PL_CONST; //variance and mean in keV converted to be in Hz
     double val=0;
+    
+    val=gsl_sf_exp(-(frequency-mean)*(frequency-mean)/(2*std*std));//dont need (1/(std*sqrt(2*M_PI))) factor since its already normalized
     
     return val;
 }
