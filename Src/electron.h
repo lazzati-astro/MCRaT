5811fce3c0d3f0a2822ce4e47b0f3e8bc6a2fa85
Tyler Parsotan <parsotat@umbc.edu>
2025-11-11 10:53:40 -0500

    Added function placeholder to sample either non-thermal electron distributions.

--- Src/electron.h
+++ Src/electron.h
@@ -6,6 +6,8 @@ void singleElectron(double *el_p, double temp, double *ph_p, gsl_rng * rand, FIL
 
 double sampleThermalElectron(double temp, gsl_rng * rand, FILE *fPtr);
 
+double sampleNonthermalElectron(double p, gsl_rng * rand, FILE *fPtr);
+
 double samplePowerLaw(double p, double gamma_min, double gamma_max, gsl_rng * rand, FILE *fPtr);
 
 double sampleDoublePowerLaw(double p1, double p2, double gamma_min, double gamma_max, double gamma_break, gsl_rng * rand, FILE *fPtr);
