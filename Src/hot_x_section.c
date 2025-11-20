23eeba7f85001eb7766b7ddbd84e54fa31c151eb
Tyler Parsotan <parsotat@umbc.edu>
2025-11-20 17:18:59 -0500

    Added missing semicolons

--- Src/hot_x_section.c
+++ Src/hot_x_section.c
@@ -47,9 +47,9 @@ double thermal_table[N_PH_E + 1][N_T + 1];
 
 void initalizeHotCrossSection(gsl_rng *rand, FILE *fPtr)
 {
-    createHotCrossSection(rand, fPtr)
+    createHotCrossSection(rand, fPtr);
 
-    readHotCrossSection(fPtr)
+    readHotCrossSection(fPtr);
 }
 
 void createHotCrossSection(gsl_rng *rand, FILE *fPtr)
