9eeeece7ea519fa56a6236f3f802fbbb88fd95c2
Tyler Parsotan <parsotat@oregonstate.edu>
2025-12-03 14:18:29 -0500

    modified addToPhotonList to add photon to location of null photon if possible to save memory.

--- Src/photons.c
+++ Src/photons.c
@@ -49,18 +49,42 @@ void reallocatePhotonListMemory(struct photonList *photon_list, int new_capacity
 
 void addToPhotonList(struct photonList *photon_list, struct photon *ph)
 {
+    int idx=0, i=0;
+    
     //add a photon to the photonList photons array
-    // If list is full, double capacity
-    if (photon_list->num_photons >= photon_list->list_capacity)
+    // If list is full, and we have no null photons to fill in then double capacity
+    if ((photon_list->num_photons >= photon_list->list_capacity) && (photon_list->num_null_photons == 0))
     {
         int new_capacity = photon_list->list_capacity * 2;
-        photon_list_resize(photon_list, new_capacity);
+        reallocatePhotonListMemory(photon_list, new_capacity);
+    }
+    
+    //if we have no null photons, just append the photon to the list
+    if (photon_list->num_null_photons == 0)
+    {
+        idx=photon_list->num_photons;
+        
+    }
+    else
+    {
+        //we need to find the null photon index and overwrite the photon there
+        for (i = 0; i < photon_list->num_photons; i++)
+        {
+            if (photon_list->photons[i].type == NULL_PHOTON)
+            {
+                idx=i;
+                i=photon_list->num_photons;
+            }
+        }
+        photon_list->num_null_photons--;
     }
     
     // Copy photon into list
-    memcpy(&photon_list->photons[photon_list->num_photons], ph, sizeof(struct photon));
+    memcpy(&(photon_list->photons)[idx], ph, sizeof(struct photon));
     photon_list->num_photons++;
     
 }
+
+
 
 
