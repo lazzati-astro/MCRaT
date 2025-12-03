6116fd5b5730ee4028f6003ffef3a1e3c512ed7d
Tyler Parsotan <parsotat@oregonstate.edu>
2025-12-03 15:19:11 -0500

    Added null photon setter to header file.

--- Src/photons.h
+++ Src/photons.h
@@ -7,3 +7,5 @@ void allocatePhotonListMemory(struct photonList *photon_list, int n_photons);
 void reallocatePhotonListMemory(struct photonList *photon_list, int new_capacity);
 
 void addToPhotonList(struct photonList *photon_list, struct photon *ph);
+
+void setNullPhoton(struct photonList *photon_list, int index);
