c8aa13e75708ba4cd988a9997b7d4edba0b2f609
Tyler Parsotan <parsotat@umbc.edu>
2026-01-06 22:43:18 -0500

    Changed gamma function that is sampled for wien function so we are sampling from the wien photon density spectrum. Verified that this gives the correct comoving frame wien spectrum.

--- Src/mclib.c
+++ Src/mclib.c
@@ -198,7 +198,7 @@ void photonInjection(struct photonList *photon_list, double r_inj, double ph_wei
                      */
                     //now sample from a gamma distribution with a=4 and b=1 (x^3*e^-x) and then convert x=h \nu / k_B*T to frequency \nu
                     //verified with simulations in python to verify this sampled function and transform does actually return a wien function for sufficiently large sample size (of 100000)
-                    fr_dum = gsl_ran_gamma(rand, 4.0, 1.0);
+                    fr_dum = gsl_ran_gamma(rand, 3.0, 1.0);
                     fr_dum*=K_B*((hydro_data->temp)[i])/PL_CONST;
                     
                 }
