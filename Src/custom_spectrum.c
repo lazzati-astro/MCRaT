427c43caa88187fae64c9332c2a5067e297ffea4
Tyler Parsotan <parsotat@umbc.edu>
2026-01-08 13:30:09 -0500

    Created custom photon sampler function to be called from the photon injection function when the user wants to inject a custom spectrum.

--- Src/custom_spectrum.c
+++ Src/custom_spectrum.c
@@ -24,3 +24,29 @@ double custom_spectrum(double frequency)
     
     return val;
 }
+
+double custom_photon_sampler(struct hydro_dataframe *hydro_data, int hydro_index, gsl_rng * rand, FILE *fPtr)
+{
+    struct photon custom_photon;
+    
+    double bb_temp=1e-8 *(GSL_CONST_CGSM_MASS_ELECTRON*GSL_CONST_CGSM_SPEED_OF_LIGHT*GSL_CONST_CGSM_SPEED_OF_LIGHT)/GSL_CONST_CGSM_BOLTZMANN;
+    double test=0;
+    double test_rand1=gsl_rng_uniform_pos(rand);
+    double test_rand2=gsl_rng_uniform_pos(rand);
+    double test_rand3=gsl_rng_uniform_pos(rand);
+    double test_rand4=gsl_rng_uniform_pos(rand);
+    double test_rand5=gsl_rng_uniform_pos(rand);
+    double test_cnt=0;
+    double fr_dum;
+    
+    while (test<M_PI*M_PI*M_PI*M_PI*test_rand1/90.0)
+    {
+        test_cnt+=1;
+        test+=1/(test_cnt*test_cnt*test_cnt*test_cnt);
+    }
+    fr_dum=-log(test_rand2*test_rand3*test_rand4*test_rand5)/test_cnt;
+    fr_dum*=K_B*bb_temp/PL_CONST;
+
+    return fr_dum;
+    
+}
