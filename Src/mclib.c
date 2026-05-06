b2d15f4213bc2d3e92178492e9aa92862c10a29e
Tyler Parsotan <parsotat@umbc.edu>
2026-05-06 09:52:38 -0400

    Changd location where the photon recalc_properties are set to 1 so both scattered/unscattered have to have their properties recalculated.

--- Src/mclib.c
+++ Src/mclib.c
@@ -1342,6 +1342,13 @@ double photonEvent(struct photonList *photon_list, double dt_max, struct hydro_d
             
             if (event_did_occur==1)
             {
+                
+                //we need to make sure that the tau for this photon gets recalculated since we have a new comoving
+                //4 momentum and the photon may no longer be in the same cell (we update the photon position before doing the scattering)
+                //this also means that the photon may be in a completely new cell by the time it scatter though this is unlikely in high density regions
+                //doing this here makes the scattered/unscatterd photons both have to have their properties recalculated
+                (ph->recalc_properties)=1;
+
                 //fprintf(fPtr,"Within the if!\n");
                 //fflush(fPtr);
                 #if SCATTERING_BIAS_SWITCH == ON
@@ -1433,11 +1440,6 @@ double photonEvent(struct photonList *photon_list, double dt_max, struct hydro_d
                 //incremement that photons number of scatterings
                 (ph->num_scatt)+=1;
                 *frame_scatt_cnt+=1; //incrememnt total number of scatterings
-
-                //we need to make sure that the tau for this photon gets recalculated since we have a new comoving
-                //4 momentum and the photon may no longer be in the same cell (we update the photon position before doing the scattering)
-                //this also means that the photon may be in a completely new cell by the time it scatter though this is unlikely in high density regions
-                (ph->recalc_properties)=1;
             
             }
                 
