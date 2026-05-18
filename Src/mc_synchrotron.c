696f9c976099fedb6d97fd16481b7f00b5cf8648
Tyler Parsotan <parsotat@umbc.edu>
2026-05-18 11:06:28 -0400

    in applyAbsorption, set photon nearest block index=-1 so we dont waste time trying to scatter a photon with an extremely low weight.

--- Src/mc_synchrotron.c
+++ Src/mc_synchrotron.c
@@ -1250,10 +1250,12 @@ void applyAbsorption(struct photon *ph, double dl)
         double tau = ph->absorption_opacity * dl;
 
         /* Guard against exp underflow for very large optical depths */
-        //set to DBL_MIN so these photons can potentially be written out instead of causing garbage data being written out in the printPhotons function. Also applyRussianRoulette, recongnises these as needing to be removed. 
+        //set to DBL_MIN so these photons can potentially be written out instead of causing garbage data being written out in the printPhotons function. Also applyRussianRoulette, recongnises these as needing to be removed.
+        //set the nearest_block_index=-1 so these photons dont scatter as we dont really care about them anymore
         if (tau > 700.0)
         {
             ph->weight = DBL_MIN;
+            ph->nearest_block_index=-1;
             return;
         }
 
